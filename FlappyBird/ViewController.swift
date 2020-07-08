6cb192d26d87a507dd8580c57e6f8ac071ec5a8e
ysanematsu <sanematsuyasuo@gmail.com>
2020-07-08 10:32:57 +0900

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by 實松保雄 on 2020/07/08.
+//  Copyright © 2020 ysanematsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
