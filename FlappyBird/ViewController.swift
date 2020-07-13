7f76aa736bb7f77d6b508b0839052691b3f9545f
ysanematsu <sanematsuyasuo@gmail.com>
2020-07-13 16:35:35 +0900

    レッスン７をコミット

--- FlappyBird/ViewController.swift
+++ FlappyBird/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import SpriteKit    //SpriteKitを使うためインポート
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //SKViewクラスのpresentScene()メソッドを使ってSKSceneを設定
+        let skView = self.view as! SKView
+        
+        //画面が１秒間に何回更新されているかを示すFPSを画面右下に表示させる
+        skView.showsFPS = true
+        
+        //ノードが幾つ表示されているかを画面右下に表示させる
+        skView.showsNodeCount = true
+        
+        //シーンを作成する。ビューと同じサイズ
+        let  scene = GameScene(size:skView.frame.size) //GameSceneクラスに変更
+        
+        //シーンをびビューに表示する
+        skView.presentScene(scene)
+        
     }
 
-
+    //ステータスバーを消す
+    override var prefersStatusBarHidden: Bool {
+        get {
+            return true
+        }
+    }
 }
 
